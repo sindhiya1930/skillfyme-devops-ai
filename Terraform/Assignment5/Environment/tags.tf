70e029610309e70ac066559a78ee1880b0aa8857
sindhiya1930 <33276851+sindhiya1930@users.noreply.github.com>
2025-05-22 13:29:10 +0530

    Delete Terraform/Assignment5/Environment directory

--- Terraform/Assignment5/Environment/tags.tf
+++
@@ -1,7 +0,0 @@
-locals {
-        common_tags = {
-                Project  = "Terraform-AWS"
-                Environment = terraform.workspace
-                Owner    = "devops-team"
-        }
-}
