f1d1239633ea4a7edee56a128ba5e0fad0abbfc4
sindhiya1930 <33276851+sindhiya1930@users.noreply.github.com>
2025-06-16 12:52:28 +0530

    Update main.tf

--- Terraform/Assignment1/EC2Instance/main.tf
+++ Terraform/Assignment1/EC2Instance/main.tf
@@ -5,8 +5,4 @@ provider "aws" {
 resource "aws_instance" "web" {
         ami = var.ami_id
         instance_type = var.instance_type
-
-        tags {
-                Name = "TerraformEC2Instamce"
-        }
 }
