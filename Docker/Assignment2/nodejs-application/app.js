1c85c6b75305412257d4533f5a4ac0c8bd2228da
sindhiya1930 <33276851+sindhiya1930@users.noreply.github.com>
2025-06-17 16:14:42 +0530

    Update app.js

--- Docker/Assignment2/nodejs-application/app.js
+++ Docker/Assignment2/nodejs-application/app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT= process.env.PORT || 3000;
 
 app.get('/', (req,res) => {
-        res.send("Hello from Nodejs App running on docker Swarm');
+        res.send("Hello from Nodejs App running on docker Swarm!!');
 });
 
 app.listen(PORT, ()=>{
