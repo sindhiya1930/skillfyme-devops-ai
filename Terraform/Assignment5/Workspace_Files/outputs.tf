ca430ab5d5ca2bca203e0b72ec5b96ee702470b2
sindhiya1930 <33276851+sindhiya1930@users.noreply.github.com>
2025-06-19 11:19:52 +0530

    Update outputs.tf

--- Terraform/Assignment5/Workspace_Files/outputs.tf
+++ Terraform/Assignment5/Workspace_Files/outputs.tf
@@ -1,21 +1,4 @@
-# outputs.tf
-
-output "ec2_instance_id" {
-  description = "The ID of the deployed EC2 instance."
-  value       = aws_instance.app_server.id
-}
-
-output "ec2_public_ip" {
-  description = "The public IP address of the deployed EC2 instance."
-  value       = aws_instance.app_server.public_ip
-}
-
-output "vpc_id" {
-  description = "The ID of the deployed VPC."
-  value       = aws_vpc.app_vpc.id
-}
-
-output "environment" {
-  description = "The environment name for which resources were deployed."
-  value       = var.environment_tag
+output "instance_public_ip" {
+        description = "The public IP"
+        value       = aws_instance.web.public_ip
 }
